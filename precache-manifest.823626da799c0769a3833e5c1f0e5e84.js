9f9eb650c3e61c4c94dd1ec548af9e4d38ef3d31
Anupam Agrawal <58875579+aanupam29@users.noreply.github.com>
2020-10-04 18:11:46 +0530

    Updates

---
+++ precache-manifest.823626da799c0769a3833e5c1f0e5e84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1123ced12fa7b6a081fe6f707ad68864",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "8517492041c3f32b8e48",
+    "url": "/recipe-app/static/css/main.e22ebbbb.chunk.css"
+  },
+  {
+    "revision": "f2bd51fcd7b838e03ab5",
+    "url": "/recipe-app/static/js/2.d4728f88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app/static/js/2.d4728f88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8517492041c3f32b8e48",
+    "url": "/recipe-app/static/js/main.4868a494.chunk.js"
+  },
+  {
+    "revision": "cd77bb1956273d314d45",
+    "url": "/recipe-app/static/js/runtime-main.14680ac4.js"
+  }
+]);
